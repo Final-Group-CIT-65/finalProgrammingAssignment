4d02cbcef3728d27181b9fd6c75c01c11188aa28
A-Most-Sincere-Gentleman <Christianluscomb@gmail.com>
2014-04-05 23:58:39 -0700

    Added some pseudo-code and laid out method/class structure.

--- midtermAssignment/src/com/example/midtermassignment/NumberCompare.java
+++ midtermAssignment/src/com/example/midtermassignment/NumberCompare.java
@@ -1,8 +1,12 @@
 package com.example.midtermassignment;
 
+import android.R.id;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NumberCompare extends Activity {
 
@@ -10,13 +14,58 @@ public class NumberCompare extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		 final Button computeButton = (Button) findViewById((Integer) R.id.button2);
+         computeButton.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+             //CALL COMPUTATION METHOD TO FIND THE LARGEST INTEGER
+            	 Num.Compute();
+             }
+         });
+         
+         final Button resetButton = (Button) findViewById((Integer) R.id.button1);
+         resetButton.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+             //CALL RESET METHOD TO CLEAR FIELDS
+            	 Num.Reset();
+             }
+         });
+         
+         final Button quitButton = (Button) findViewById((Integer) R.id.button3);
+         quitButton.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+             //QUIT PROGRAM
+            	 Num.Quit();
+             }
+         });
+
+	}
+	
+	
+	public static class Num {
+		
+		//This is the bitch class where the program gets laid
+				public static void Compute(){
+										
+
+					
+		}
+		
+				public static void Reset(){
+			
+		}
+		
+				public static void Quit(){
+			
+		}
+		
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.number_compare, menu);
 		return true;
 	}
-
 }
