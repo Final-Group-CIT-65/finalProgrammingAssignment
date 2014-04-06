410c82212ab53d4b2997ce814e2389e6f9deb7a0
A-Most-Sincere-Gentleman <Christianluscomb@gmail.com>
2014-04-06 15:44:41 -0700

    Made changes to XML formatting and limited user input to avoid exceeding integer range.
    Set up input parsing and printing. Program is now ready for sorting
    algorithm and is
    virtually bug free.

--- midtermAssignment/src/com/example/midtermassignment/NumberCompare.java
+++ midtermAssignment/src/com/example/midtermassignment/NumberCompare.java
@@ -7,9 +7,29 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NumberCompare extends Activity {
 
+	//Variables relevant to input
+	
+	//Buttons
+	public Button ComputeButton;
+	public Button ResetButton;
+	public Button QuitButton;
+	
+	//User input text fields
+	private EditText editText1;
+	private EditText editText2;
+	private EditText editText3;
+	
+	// Integer values of input strings
+	int Field1_Int;
+	int Field2_Int;
+	int Field3_Int;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,9 +39,17 @@ public class NumberCompare extends Activity {
          computeButton.setOnClickListener(new View.OnClickListener() {
              public void onClick(View v) {
              //CALL COMPUTATION METHOD TO FIND THE LARGEST INTEGER
-            	 System.out.print("Testing 123");
-            	 //TODO: Create if statement that checks if all fields have a value
-            	 Num.Compute();
+            	 if(Integer.toString(R.id.editText1) != null && Integer.toString(R.id.editText2) != null && Integer.toString(R.id.editText3) != null){
+            		 
+            	 System.out.println("Testing 123");
+            	 Compute();
+            	 
+            	 }
+            	 else{
+            		 
+            		 Toast.makeText(getBaseContext(), "Please fill out all three fields!",
+								Toast.LENGTH_LONG).show();
+            	 }
              }
          });
          
@@ -29,7 +57,7 @@ public class NumberCompare extends Activity {
          resetButton.setOnClickListener(new View.OnClickListener() {
              public void onClick(View v) {
              //CALL RESET METHOD TO CLEAR FIELDS
-            	 Num.Reset();
+            	 Reset();
              }
          });
          
@@ -37,36 +65,80 @@ public class NumberCompare extends Activity {
          quitButton.setOnClickListener(new View.OnClickListener() {
              public void onClick(View v) {
              //QUIT PROGRAM
-            	 Num.Quit();
+            	 Quit();
              }
          });
+         
+         
 
 	}
 	
 	
-	public static class Num {
-		
-		//This is the bitch class where the program gets laid
-				public static void Compute(){
+
+	
 				
-					String InputBox1 = Integer.toString(R.id.textView1);
-					String InputBox2 = Integer.toString(R.id.textView2);
-					String InputBox3 = Integer.toString(R.id.textView3);
-					
-					System.out.println(InputBox1+InputBox2+InputBox3);
-					
-					
-		}
-		
-				public static void Reset(){
+				//This method is called after Reset is pressed
+						public void Reset(){
 			
-		}
+					}
+				
+				
+				
+				//This method is called after Quit is pressed
+						public void Quit(){
+
+							}
+	
+					
+				
+					
+			public void Compute(){
+				
+					//Acquire user input from editText form widgets (SORRY FOR AMBIGUOUS SAMENAME, I JUST CAUGHT IT)	
+					editText1 = (EditText) findViewById(R.id.editText1);
+					editText2 = (EditText) findViewById(R.id.editText2);
+					editText3 = (EditText) findViewById(R.id.editText3);
+					
+					// Assigns input to string values.
+					String Field1 = editText1.getText().toString();
+					
+					
+					String Field2 = editText2.getText().toString();
+					
+					
+					String Field3 = editText3.getText().toString();
+					
+					//Protip: Make sure to refer to these variables by their given integer values and not their string values.
+					
+						//Checks if strings are of appropriate length and then parses them to three separate integers.
+						 Field1_Int = Integer.parseInt(Field1);
+						 Field2_Int = Integer.parseInt(Field2);
+						 Field3_Int = Integer.parseInt(Field3);
+						 
+						 
+							//PUT THE SORTING THING HERE
+							
+							//This collects the three numbers and spits them out in a toast notification.
+							String TextCompilation = ("Your nums are "+Field1_Int+", "+Field2_Int+", and "+Field3_Int);
+							
+							Toast.makeText(getBaseContext(), TextCompilation,
+									Toast.LENGTH_LONG).show();
+							
 		
-				public static void Quit(){
+							
+						//	int LargestNumber = LARGEST NUMBER AS DETERMINED BY SORTING ALGORITHM;
+							
+							final TextView mTextView = (TextView) findViewById(R.id.textView4);
+						mTextView.setText(String.valueOf(Field3_Int));
+					
+					
+					
 			
+					
 		}
 		
-	}
+				
+	
 
 	
 	@Override
