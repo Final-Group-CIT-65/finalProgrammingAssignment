6d92e4910a8c375d7e93ff3362caf0a140d51e3a
Nicholas Roberts <nick.roberts.127.0.0.1@gmail.com>
2014-04-05 22:29:04 -0700

    More or less done with GUI and string resources.

--- midtermAssignment/gen/com/example/midtermassignment/R.java
+++ midtermAssignment/gen/com/example/midtermassignment/R.java
@@ -22,11 +22,20 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int nums_logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f08000a;
+        public static final int button1=0x7f080006;
+        public static final int button2=0x7f080007;
+        public static final int button3=0x7f080005;
+        public static final int editText1=0x7f080002;
+        public static final int editText2=0x7f080003;
+        public static final int editText3=0x7f080004;
         public static final int textView1=0x7f080001;
         public static final int textView2=0x7f080000;
+        public static final int textView3=0x7f080008;
+        public static final int textView4=0x7f080009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -37,7 +46,18 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int computeButton=0x7f050007;
+        /**  THE UKRAINE THE UKRAINE 
+         */
+        public static final int outputCaption=0x7f050004;
+        public static final int outputStream=0x7f050005;
+        public static final int quitButton=0x7f050008;
+        /**  THE CRIMEA THE CRIMEAN PENINSULASJIOEWF 
+         */
+        public static final int resetButton=0x7f050006;
         public static final int subCaption=0x7f050003;
+        /**  THE RUSSIA 
+         */
         public static final int topMostCaption=0x7f050002;
     }
     public static final class style {
