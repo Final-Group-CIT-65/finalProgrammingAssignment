8227076e13d411b4a2f96a2f92900489f4427191
nick11robers <nick.roberts.127.0.0.1@gmail.com>
2014-05-20 12:36:49 -0700

    Every button does as it should with the exception of '=' which is not yet implemented. '=' should convert the string to an expression and do the calculation. There is also the problem of text overflow, so perhaps there can be a string length limit.

--- src/com/example/finalprogrammingassignment/NumberCompare.java
+++ src/com/example/finalprogrammingassignment/NumberCompare.java
@@ -2,35 +2,46 @@ package com.example.finalprogrammingassignment;
 
 import com.example.finalprogrammingassignment.R;
 
-import android.R.id;
+//import android.R.id;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class NumberCompare extends Activity {
 
-	//Variables relevant to input
+	//Final variables relevant to input and output.
+	//These variables are global because they are fundamentally relevant to the entire program.
+		//Also, they're final, so it's kind of like having global constants. 
 	
-	//Buttons
-	public Button ComputeButton;
-	public Button ResetButton;
-	public Button QuitButton;
 	
-	//User input text fields
-	private EditText editText1;
-	private EditText editText2;
-	private EditText editText3;
+	public Button buttonZero;
+	public Button buttonOne;
+	public Button buttonTwo;
+	public Button buttonThree;
+	public Button buttonFour;
+	public Button buttonFive;
+	public Button buttonSix;
+	public Button buttonSeven;
+	public Button buttonEight;
+	public Button buttonNine;
+	public Button buttonAdd;
+	public Button buttonSubtract;
+	public Button buttonMultiply;
+	public Button buttonDivide;
+	public Button buttonReset;
+	public Button buttonDelete;
 	
-	// Integer values of input strings
-	int Field1_Int;
-	int Field2_Int;
-	int Field3_Int;
-	int FieldTemp_Int;
+	//Output string
+		//This variable is not final or constant because it is very much relevant to the entire program,
+		//it is changed throughout the program,
+		//and thus it's scope is global to the program. 
+		//Excuse me while I ask the gods of good programming practices for forgiveness. 
+	String textOut = "";
 	
 	
 	
@@ -41,154 +52,202 @@ public class NumberCompare extends Activity {
 		setContentView(R.layout.main);
 		
 		
+		//Global output field
+		final TextView outputField = (TextView) findViewById((Integer)R.id.outputField);
+		
+		//Global Buttons
+		final Button buttonZero = (Button) findViewById((Integer) R.id.buttonZero);
+		final Button buttonDecimal = (Button) findViewById((Integer)R.id.buttonDecimal);
+		final Button buttonEquals = (Button) findViewById((Integer)R.id.buttonEquals);
+		final Button buttonDivide = (Button) findViewById((Integer)R.id.buttonDivide);
+		
+		final Button buttonOne = (Button) findViewById((Integer)R.id.buttonOne);
+	    final Button buttonTwo = (Button) findViewById((Integer)R.id.buttonTwo);
+		final Button buttonThree = (Button) findViewById((Integer)R.id.buttonThree);
+		final Button buttonMultiply = (Button) findViewById((Integer)R.id.buttonMultiply);
+		
+		final Button buttonFour = (Button) findViewById((Integer)R.id.buttonFour);
+		final Button buttonFive = (Button) findViewById((Integer)R.id.buttonFive);
+		final Button buttonSix = (Button) findViewById((Integer)R.id.buttonSix);
+		final Button buttonSubtract = (Button) findViewById((Integer)R.id.buttonSubtract);
+		
+		final Button buttonSeven = (Button) findViewById((Integer)R.id.buttonSeven);
+		final Button buttonEight = (Button) findViewById((Integer)R.id.buttonEight);
+		final Button buttonNine = (Button) findViewById((Integer)R.id.buttonNine);
+		final Button buttonAdd = (Button) findViewById((Integer)R.id.buttonAdd);
+		
+		final Button buttonReset = (Button) findViewById((Integer)R.id.buttonReset);
+		final Button buttonDelete = (Button) findViewById((Integer)R.id.buttonDelete);
 		
 		
+		buttonZero.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '0' to the string //
+            	textOut+='0';
+            	
+           		outputField.setText(textOut);
+            }
+        });
 		
+		buttonDecimal.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '.' to the string //
+            	textOut+='.';
+            	
+           		outputField.setText(textOut);
+            }
+        });
 		
-		/*
-		 final Button computeButton = (Button) findViewById((Integer) R.id.button2);
-         computeButton.setOnClickListener(new View.OnClickListener() {
-             public void onClick(View v) {
-             //CALL COMPUTATION METHOD TO FIND THE LARGEST INTEGER
-            	 	///////////////////
-            	 editText1 = (EditText) findViewById(R.id.editText1);
-            	 String Field1 = editText1.getText().toString().trim();
-            	 
-            	 editText2 = (EditText) findViewById(R.id.editText2);
-            	 String Field2 = editText2.getText().toString().trim();
-            	 
-            	 editText3 = (EditText) findViewById(R.id.editText3);
-            	 String Field3 = editText3.getText().toString().trim();
-            	 
-            	 if((Field1.length() >= 1) && (Field2.length() >= 1) && (Field3.length() >= 1)){
-            		 Compute();
-            	 }
-            	 
-            	 else{
-            		
-            		 final TextView LargestNumOutput = (TextView) findViewById(R.id.textView4);
-            		 LargestNumOutput.setText("No. Enter THREE nums. ");
-            	 }
-
-             }
-         });
-         
-         final Button resetButton = (Button) findViewById((Integer) R.id.button1);
-         resetButton.setOnClickListener(new View.OnClickListener() {
-             public void onClick(View v) {
-             //CALL RESET METHOD TO CLEAR FIELDS
-            	 Reset();
-             }
-         });
-         
-         final Button quitButton = (Button) findViewById((Integer) R.id.button3);
-         quitButton.setOnClickListener(new View.OnClickListener() {
-             public void onClick(View v) {
-             //QUIT PROGRAM
-            	 Minimize();
-             }
-         });
-         */
+		buttonEquals.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Call calculation method //
+            	
+           		outputField.setText(textOut);
+            }
+        });
 		
+		buttonDivide.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '/' to the string //
+            	textOut+='/';
+            	
+           		outputField.setText(textOut);
+            }
+        });
 		
+		buttonOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '1' to the string //
+            	textOut+='1';
+            	
+           		outputField.setText(textOut);
+            }
+        });
 		
+		buttonTwo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '2' to the string //
+            	textOut+='2';
+            	
+           		outputField.setText(textOut);
+            }
+        });
 		
+		buttonThree.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '3' to the string //
+            	textOut+='3';
+            	
+           		outputField.setText(textOut);
+            }
+        });
 		
+		buttonMultiply.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '*' to the string //
+            	textOut+='*';
+            	
+           		outputField.setText(textOut);
+            }
+        });
+		
+		buttonFour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '4' to the string //
+            	textOut+='4';
+            	
+           		outputField.setText(textOut);
+            }
+        });
+		
+		buttonFive.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '5' to the string //
+            	textOut+='5';
+            	
+           		outputField.setText(textOut);
+            }
+        });
+		
+		buttonSix.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '6' to the string //
+            	textOut+='6';
+            	
+           		outputField.setText(textOut);
+            }
+        });
+		
+		buttonSubtract.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '-' to the string //
+            	textOut+='-';
+            	
+           		outputField.setText(textOut);
+            }
+        });
+		
+		buttonSeven.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '7' to the string //
+            	textOut+='7';
+            	
+           		outputField.setText(textOut);
+            }
+        });
+		
+		buttonEight.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '8' to the string //
+            	textOut+='8';
+            	
+           		outputField.setText(textOut);
+            }
+        });
+		
+		buttonNine.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '9' to the string //
+            	textOut+='9';
+            	
+           		outputField.setText(textOut);
+            }
+        });
+		
+		buttonAdd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Append a '+' to the string //
+            	textOut+='+';
+            	
+           		outputField.setText(textOut);
+            }
+        });
+		
+		buttonReset.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Clear the string //
+            	textOut ="";
+            	
+           		outputField.setText(textOut);
+            }
+        });
+		
+		buttonDelete.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// Remove last character from the string if string is not empty //
+            	if(textOut.length()>=1)
+            		textOut = textOut.substring(0, textOut.length() - 1);
+            	
+           		outputField.setText(textOut);
+            }
+        });
+		
+			
          
 
 	}
 	
 	
-
-	
-				
-				//This method is called after Reset is pressed
-					public void Reset(){
-			
-							/*
-							final TextView Field1Reset = (TextView) findViewById(R.id.editText1);
-							Field1Reset.setText(String.valueOf(""));
-							
-							final TextView Field2Reset = (TextView) findViewById(R.id.editText2);
-							Field2Reset.setText(String.valueOf(""));
-							
-							final TextView Field3Reset = (TextView) findViewById(R.id.editText3);
-							Field3Reset.setText(String.valueOf(""));
-							
-							final TextView LargestNumOutput = (TextView) findViewById(R.id.textView4);
-							LargestNumOutput.setText(R.string.outputStream);
-							*/
-					}
-				
-				
-				
-				//This method is called after Quit is pressed
-						public void Minimize(){
-						
-								finish();
-							}
-	
-					
-				
-					
-			public void Compute(){
-				/*
-					//Acquire user input from editText form widgets (SORRY FOR AMBIGUOUS SAMENAME, I JUST CAUGHT IT)	
-					editText1 = (EditText) findViewById(R.id.editText1);
-					editText2 = (EditText) findViewById(R.id.editText2);
-					editText3 = (EditText) findViewById(R.id.editText3);
-					
-					// Assigns input to string values.
-					String Field1 = editText1.getText().toString();
-					
-					
-					String Field2 = editText2.getText().toString();
-					
-					
-					String Field3 = editText3.getText().toString();
-					
-					//Protip: Make sure to refer to these variables by their given integer values and not their string values.
-					
-						//Checks if strings are of appropriate length and then parses them to three separate integers.
-						 Field1_Int = Integer.parseInt(Field1);
-						 Field2_Int = Integer.parseInt(Field2);
-						 Field3_Int = Integer.parseInt(Field3);
-						 
-						 
-							//PUT THE SORTING THING HERE
-						 if(Field1_Int >= Field2_Int && Field1_Int>= Field3_Int)
-							 FieldTemp_Int = Field1_Int; 
-						 else if(Field2_Int >= Field1_Int && Field2_Int>= Field3_Int)
-							 FieldTemp_Int = Field2_Int;
-						 else if(Field3_Int >= Field1_Int && Field3_Int> Field2_Int)
-							 FieldTemp_Int = Field3_Int;
-						 // ^three main cases
-						 else if(Field1_Int == Field2_Int && Field1_Int == Field3_Int)
-							 FieldTemp_Int = Field1_Int;
-						 // if they are all equal. 
-						 else
-							 FieldTemp_Int = 0; 
-						 // This should never happen. 
-						 
-							
-							//This collects the three numbers and spits them out in a toast notification.
-							String TextCompilation = ("Your nums are "+Field1_Int+", "+Field2_Int+", and "+Field3_Int);
-							
-							Toast.makeText(getBaseContext(), TextCompilation,
-									Toast.LENGTH_LONG).show();
-							
-		
-							
-						//	int LargestNumber = LARGEST NUMBER AS DETERMINED BY SORTING ALGORITHM;
-							
-							final TextView LargestNumOutput = (TextView) findViewById(R.id.textView4);
-						LargestNumOutput.setText(String.valueOf(FieldTemp_Int));
-					
-					
-					*/
-			
-					
-		}
 		
 				
 	
